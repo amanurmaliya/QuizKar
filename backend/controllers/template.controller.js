2c228c0ccf762b48873e646b700ceaa0bdfc93c3
amanurmaliya <amanurmaliya609@gmail.com>
2025-07-12 21:25:17 +0530

    Create get template by id and delete by id controllers

--- backend/controllers/template.controller.js
+++ backend/controllers/template.controller.js
@@ -2,11 +2,11 @@ const {Template} = require("../models/index.model")
 
 const getAllTemplates = async(req, res, next) => {
     try{
-        const Templates = await Template.find();
+        const templates = await Template.find();
 
         return res.status(200).json({
             success : true,
-            Templates
+            data : templates
         })
     }
     catch(e)
@@ -18,32 +18,49 @@ const getAllTemplates = async(req, res, next) => {
     }
 }
 
-const getTemplateById = async(req, res)=> {
+const getTemplateById = async(req, res, next)=> {
     try{
-
+        const {id} = req.params;
+        if(!id)
+        {
+            const error = new Error("No id found template id is required", {cause: new Error("Template id is required")})
+            return next(error)
+        }
+        const template = await Template.findById(id )
         return res.status(200).json({
             success : true,
-            message : "Data fetched SuccessFully!",
-            data
+            message : "Data of the user fetched SuccessFully!",
+            data : template
         })
-    }catch(error){
-        return res.status(500).json({
-            success : false,
-            message : "Sorry Failed to Get the user data due to server error. Kindly Try again later",
-            error
-        })
+    }catch(e)
+    {
+        const error = new Error("Failed To Fetch the template by id",{
+            cause : e // This will display the exact reason for the error
+        });
+        return next(error) 
     }
 }
 
-const deleteTemplateById = async(req, res) => {
+const deleteTemplateById = async(req, res, next) => {
+    const {id} = req.params;
     try{
-
-    }catch(error){
-        return res.status(500).json({
-            success : false,
-            message : "Sorry Failed to Get the user data due to server error. Kindly Try again later",
-            error
+        if(!id)
+        {
+            const error = new Error("No id found template id is required", {cause: new Error("Template id is required")})
+            return next(error)
+        }
+        const template = await Template.findByIdAndDelete(id )
+        return res.status(200).json({
+            success : true,
+            message : "Data of the user fetched SuccessFully!",
+            data : template
         })
+    }catch(e)
+    {
+        const error = new Error(`Failed To delete the templates with id : ${id}`,{
+            cause : e // This will display the exact reason for the error
+        });
+        return next(error) 
     }
 }
 
